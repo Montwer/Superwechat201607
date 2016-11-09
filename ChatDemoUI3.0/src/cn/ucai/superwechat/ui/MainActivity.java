92898c0e196bba0d34fa6c8845b86e0e3cac1bf5
LSL <94891071@qq.com>
2016-11-09 18:54:56 +0800

    更新会话链表

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/MainActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/MainActivity.java
@@ -26,7 +26,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.PowerManager;
 import android.provider.Settings;
-import android.support.annotation.NonNull;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v4.view.ViewPager;
 import android.view.KeyEvent;
@@ -673,9 +672,5 @@ public class MainActivity extends BaseActivity implements DMTabHost.OnCheckedCha
         registerReceiver(internalDebugReceiver, filter);
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        PermissionsManager.getInstance().notifyPermissionsChange(permissions, grantResults);
-    }
+  
 }
